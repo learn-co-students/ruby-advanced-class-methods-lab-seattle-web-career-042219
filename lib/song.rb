9da2ffceb6ea1882868a28ccb0dcb1a222ebdf12
github username <github email address>
2019-04-25 16:18:27 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,63 @@ class Song
     @@all
   end
 
+  def self.create
+  	song = self.new
+  	self.all << song
+  	song
+  end
+
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song
+  end
+
+  def self.create_by_name(name)
+  	song = self.new
+  	song.name = name
+  	self.all << song
+  	song
+  end
+
+  def self.find_by_name(name)
+  	self.all.find do |song|
+  		song.name == name
+  	end
+  end
+
+  def self.find_or_create_by_name(name)
+  	self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+  	self.all.sort_by do |song|
+  		song.name
+  	end
+  end
+
+  def self.new_from_filename(file_name)
+  	song = self.new
+  	name_formated = file_name.split(/\s-\s|[.]/)
+  	song.name = name_formated[1]
+  	song.artist_name = name_formated[0]
+  	song
+  end
+
+  def self.create_from_filename(file_name)
+  	song = self.new
+  	name_formated = file_name.split(/\s-\s|[.]/)
+  	song.name = name_formated[1]
+  	song.artist_name = name_formated[0]
+  	self.all << song
+  end
+
+  def self.destroy_all
+  	self.all.clear
+  end
+
   def save
     self.class.all << self
   end
-
 end
+
